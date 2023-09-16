83ffa8b46b98cb77814add3e45c9e7269bc8e3d5
unknown <emkhan89>
2023-09-16 22:20:40 +0100

    add footer with social handles and copyright message

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap"; // Import all of Bootstrap's JS
 
@@ -9,6 +10,7 @@ function App() {
     <div className="container">
       <Navbar />
       <Jumbotron />
+      <Footer />
     </div>
   );
 }
