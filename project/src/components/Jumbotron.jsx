9d54901bfbaa00157dc8b4e4a2859ad488471343
unknown <emkhan89>
2023-09-16 22:44:23 +0100

    add avatar to jumbotron

--- project/src/components/Jumbotron.jsx
+++ project/src/components/Jumbotron.jsx
@@ -4,8 +4,16 @@ function Jumbotron() {
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold">Hello, world!</h1>
-        <p className="col-md-8 fs-4">I'm a jumbotron!</p>
+        <img
+          src="./src/assets/mohsin-avatar.png"
+          alt="mohsin-avatar"
+          id="avatar"
+        />
+        <h1 className="display-5 fw-bold">Hello World, I'm Mohsin!</h1>
+
+        <h3>
+          <b>I'm a Junior Front End Web Developer!</b>
+        </h3>
         <button className="btn btn-primary btn-lg" type="button">
           Example button
         </button>
