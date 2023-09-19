f58ead506261eadade5c7f257566e05824109d59
unknown <emkhan89>
2023-09-19 21:26:26 +0100

    add bootstrap cards

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,14 +4,42 @@ import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Footer from "./components/Footer/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap"; // Import all of Bootstrap's JS
-import ChallengesCard from "./components/ChallengesCard/index";
+// import ChallengesCard from "./components/ChallengesCard/index";
 // import challenges from "./challenges.json";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+
+import friends from "./friends.json";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
       <Jumbotron />
+      <h3 id="work">
+        <b>Work</b>
+      </h3>
+      <Wrapper>
+        <FriendCard
+          name={friends[0].name}
+          image={friends[0].image}
+          description={friends[0].description}
+          location={friends[0].location}
+        />
+        <FriendCard
+          name={friends[1].name}
+          image={friends[1].image}
+          description={friends[1].description}
+          location={friends[1].location}
+        />
+        <FriendCard
+          name={friends[2].name}
+          image={friends[2].image}
+          description={friends[2].description}
+          location={friends[2].location}
+        />
+      </Wrapper>
+
       <Footer />
     </div>
   );
--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#about">
+            <a className="nav-link" href="#avatar">
               <h4>
                 <b>About Me</b>
               </h4>
